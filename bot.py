e4f5385ff9e27ecdc32ab380076315e0b2b7f5bb
Vincent Cunningham <v.cunningham@flagrama.com>
2018-08-20 20:55:51 -0400

    add debug flag

--- bot.py
+++ bot.py
@@ -6,32 +6,50 @@ import irc.bot
 import requests
 
 class TwitchBot(irc.bot.SingleServerIRCBot):
-    def __init__(self):
-        logging.basicConfig()
-        self.logger = logging.getLogger('TwitchBot')
-        self.logger.setLevel(logging.INFO)
+    def __init__(self, debug):
+        self.init_logging(debug)
 
         self.client_id = os.environ['TWITCH_ID']
         self.token = os.environ['TWITCH_TOKEN']
         username = os.environ['TWITCH_BOT_NAME']
-        channel = os.environ['TWITCH_CHANNEL']
-        self.channel = '#%s' % channel
+        self.channel_name = os.environ['TWITCH_CHANNEL']
+        self.channel = '#%s' % self.channel_name
 
-        url = 'https://api.twitch.tv/helix/users?login=%s' % channel
+        self.get_channel_id
+        self.irc_connect(username)
+
+    # Methods
+    def init_logging(self, debug):
+        logging.basicConfig()
+        self.logger = logging.getLogger(__name__)
+        if debug:
+            self.logger.setLevel(logging.DEBUG)
+        else:
+            self.logger.setLevel(logging.INFO)
+
+    def irc_connect(self, username):
+        server = 'irc.chat.twitch.tv'
+        port = 6667
+        self.logger.info('Connecting to %s on port %s...' % (server, port))
+        irc.bot.SingleServerIRCBot.__init__(self, [(server, port, self.token)], username, username)
+        self.logger.info('Connecting to database...')
+
+    def get_channel_id(self):
+        url = 'https://api.twitch.tv/helix/users?login=%s' % self.channel_name
         headers = {'Client-ID': self.client_id}
         r = requests.get(url, headers=headers).json()
 
         self.channel_id = r['data'][0]['id']
 
-        server = 'irc.chat.twitch.tv'
-        port = 6667
-        self.logger.info('Connecting to %s on port %s...' % (server, port))
-        irc.bot.SingleServerIRCBot.__init__(self, [(server, port, self.token)], username, username)
-    
-    def on_welcome(self, c, e):
+    # Events
+    def on_welcome(self, connection, event):
         self.logger.info('Joining %s' % self.channel)
+        self.logger.debug(event)
 
-        c.cap('REQ', ':twitch.tv/membership')
-        c.cap('REQ', ':twitch.tv/tags')
-        c.cap('REQ', ':twitch.tv/commands')
-        c.join(self.channel)
+        connection.cap('REQ', ':twitch.tv/membership')
+        connection.cap('REQ', ':twitch.tv/tags')
+        connection.cap('REQ', ':twitch.tv/commands')
+        connection.join(self.channel)
+
+    def on_pubmsg(self, connection, event):
+        self.logger.debug(event)
