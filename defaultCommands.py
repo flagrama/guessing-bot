d3ed92d88ff8ac61434f5af55ee64e6320d032cb
Vincent Cunningham <v.cunningham@flagrama.com>
2018-08-23 14:55:47 -0400

    disallow editcom from creating empty commands
    fixes #20

--- defaultCommands.py
+++ defaultCommands.py
@@ -1,73 +1,90 @@
 import customCommands
 
 def do_default_command(twitch_bot, connection, command):
-        if command[0] == '!addcom':
-            twitch_bot.logger.debug('Add Command command received')
+    try:
+        command_name = command[0]
+        custom_command_name = command[1]
+    except IndexError:
+        message = 'Custom command name not provided'
+        twitch_bot.logger.error(message)
+        connection.privmsg(twitch_bot.channel, message)
+        return
 
-            # Command exists
-            if command[1] in twitch_bot.commands:
-                message = 'Command %s already exists' % command[1]
-                twitch_bot.logger.info(message)
-                connection.privmsg(twitch_bot.channel, message)
+    if command_name == '!addcom':
+        twitch_bot.logger.debug('Add Command command received')
 
-            # New command is valid
-            elif len(command) > 2:
-                message = 'Added custom command %s' % command[1]
-                customCommands.add_command(twitch_bot.streamer, command[1], command[2:])
-                twitch_bot.commands += [command[1]]
-                twitch_bot.logger.info(message + ' with output %s' % ' '.join(command[2:]))
-                connection.privmsg(twitch_bot.channel, message)
-                twitch_bot.logger.debug(twitch_bot.commands)
+        # Command exists
+        if custom_command_name in twitch_bot.commands:
+            message = 'Command %s already exists' % custom_command_name
+            twitch_bot.logger.info(message)
+            connection.privmsg(twitch_bot.channel, message)
 
-            # New command doesn't have output
-            else:
-                message = 'Custom commands require a message to send to chat when called'
-                twitch_bot.logger.info(message)
-                connection.privmsg(twitch_bot.channel, message)
+        # New command is valid
+        elif len(command) > 2:
+            message = 'Added custom command %s' % custom_command_name
+            customCommands.add_command(twitch_bot.streamer, custom_command_name, command[2:])
+            twitch_bot.commands += [custom_command_name]
+            twitch_bot.logger.info(message + ' with output %s' % ' '.join(command[2:]))
+            connection.privmsg(twitch_bot.channel, message)
+            twitch_bot.logger.debug(twitch_bot.commands)
 
-        elif command[0] == '!delcom':
-            twitch_bot.logger.debug('Delete Command command received')
+        # New command doesn't have output
+        else:
+            message = 'Custom commands require a message to send to chat when called'
+            twitch_bot.logger.info(message)
+            connection.privmsg(twitch_bot.channel, message)
 
-            # Built-in command cannot be deleted
-            if command[1] in twitch_bot.default_commands:
-                message = 'Command %s cannot be removed' % command[1]
-                twitch_bot.logger.info(message)
-                connection.privmsg(twitch_bot.channel, message)
+    elif command_name == '!delcom':
+        twitch_bot.logger.debug('Delete Command command received')
 
-            # Command doesn't exist
-            elif not command[1] in twitch_bot.commands:
-                message = 'Command %s does not exist' % command[1]
-                twitch_bot.logger.info(message)
-                connection.privmsg(twitch_bot.channel, message)
+        # Built-in command cannot be deleted
+        if custom_command_name in twitch_bot.default_commands:
+            message = 'Command %s cannot be removed' % custom_command_name
+            twitch_bot.logger.info(message)
+            connection.privmsg(twitch_bot.channel, message)
 
-            # Delete given command
-            else:
-                message = 'Removed custom command %s' % command[1]
-                customCommands.remove_command(twitch_bot.streamer, command[1])
-                twitch_bot.commands.remove(command[1])
-                twitch_bot.logger.info(message)
-                connection.privmsg(twitch_bot.channel, message)
-                twitch_bot.logger.debug(twitch_bot.commands)
+        # Command doesn't exist
+        elif not custom_command_name in twitch_bot.commands:
+            message = 'Command %s does not exist' % custom_command_name
+            twitch_bot.logger.info(message)
+            connection.privmsg(twitch_bot.channel, message)
 
-        elif command[0] == '!editcom':
-            twitch_bot.logger.debug('Edit Command command received')
+        # Delete given command
+        else:
+            message = 'Removed custom command %s' % custom_command_name
+            customCommands.remove_command(twitch_bot.streamer, custom_command_name)
+            twitch_bot.commands.remove(custom_command_name)
+            twitch_bot.logger.info(message)
+            connection.privmsg(twitch_bot.channel, message)
+            twitch_bot.logger.debug(twitch_bot.commands)
 
-            # Built-in commands cannot be edited
-            if command[1] in twitch_bot.default_commands:
-                message = 'Command %s cannot be edited' % command[1]
-                twitch_bot.logger.info(message)
-                connection.privmsg(twitch_bot.channel, message)
+    elif command_name == '!editcom':
+        twitch_bot.logger.debug('Edit Command command received')
 
-            # Command doesn't exist
-            elif not command[1] in twitch_bot.commands:
-                message = 'Command %s does not exist' % command[1]
-                twitch_bot.logger.info(message)
-                connection.privmsg(twitch_bot.channel, message)
+        # Built-in commands cannot be edited
+        if custom_command_name in twitch_bot.default_commands:
+            message = 'Command %s cannot be edited' % custom_command_name
+            twitch_bot.logger.info(message)
+            connection.privmsg(twitch_bot.channel, message)
 
-            # Edit given command
-            else:
-                message = 'Edited custom command %s' % command[1]
-                customCommands.edit_command(twitch_bot.streamer, command[1], command[2:])
-                twitch_bot.logger.info(message)
-                connection.privmsg(twitch_bot.channel, message)
-                twitch_bot.logger.debug(twitch_bot.commands)
+        # Command doesn't exist
+        elif not custom_command_name in twitch_bot.commands:
+            message = 'Command %s does not exist' % custom_command_name
+            twitch_bot.logger.info(message)
+            connection.privmsg(twitch_bot.channel, message)
+
+
+
+        # Edited command is valid
+        elif len(command) > 2:
+            message = 'Edited custom command %s' % custom_command_name
+            customCommands.edit_command(twitch_bot.streamer, custom_command_name, command[2:])
+            twitch_bot.logger.info(message)
+            connection.privmsg(twitch_bot.channel, message)
+            twitch_bot.logger.debug(twitch_bot.commands)
+
+        # Edited command doesn't have output
+        else:
+            message = 'Custom commands require a message to send to chat when called'
+            twitch_bot.logger.info(message)
+            connection.privmsg(twitch_bot.channel, message)
