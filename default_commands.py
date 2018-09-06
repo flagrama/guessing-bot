42683485fdaf63acbe15f738b9c4ce75888d7ad5
Vincent Cunningham <v.cunningham@flagrama.com>
2018-09-05 22:12:21 -0400

    make default_commands more compact

--- default_commands.py
+++ default_commands.py
@@ -1,4 +1,6 @@
+"""Contains the functions and classes of the bot's basic commands."""
 import logging
+from functools import partial
 import custom_commands
 import settings
 
@@ -9,88 +11,103 @@ if settings.DEBUG:
 else:
     LOGGER.setLevel(logging.INFO)
 
+def add_command(bot, command):
+    """Adds a custom command to the database."""
+    LOGGER.debug('Add Command command received')
+
+    # Command exists
+    if (command[1] in bot.commands['default']
+            + bot.commands['guessing-game']):
+        message = 'Command %s already exists' % command[1]
+        LOGGER.info(message)
+        return message
+
+    # New command is valid
+    if len(command) > 2:
+        message = 'Added custom command %s' % command[1]
+        custom_commands.add_command(bot.streamer, command[1], command[2:])
+        bot.commands['custom'] += [command[1]]
+        LOGGER.info('%s with output %s', message, ' '.join(command[2:]))
+        LOGGER.debug(bot.commands['custom'])
+        return message
+
+    # New command doesn't have output
+    message = 'Custom commands require a message to send to chat when called'
+    LOGGER.info(message)
+    return message
+
+def delete_command(bot, command):
+    """Deletes a custom command from the database."""
+    LOGGER.debug('Delete Command command received')
+
+    # Built-in command cannot be deleted
+    if (command[1] in bot.commands['default']
+            + bot.commands['guessing-game']):
+        message = 'Command %s cannot be removed' % command[1]
+        LOGGER.info(message)
+        return message
+
+    # Command doesn't exist
+    if not command[1] in bot.commands['custom']:
+        message = 'Command %s does not exist' % command[1]
+        LOGGER.info(message)
+        return message
+
+    # Delete given command
+    message = 'Removed custom command %s' % command[1]
+    custom_commands.remove_command(bot.streamer, command[1])
+    bot.commands['custom'].remove(command[1])
+    LOGGER.info(message)
+    LOGGER.debug(bot.commands['custom'])
+    return message
+
+def edit_command(bot, command):
+    """Edits a custom command in the database."""
+    LOGGER.debug('Edit Command command received')
+
+    # Built-in commands cannot be edited
+    if (command[1] in bot.commands['default']
+            + bot.commands['guessing-game']):
+        message = 'Command %s cannot be edited' % command[1]
+        LOGGER.info(message)
+        return message
+
+    # Command doesn't exist
+    if not command[1] in bot.commands['custom']:
+        message = 'Command %s does not exist' % command[1]
+        LOGGER.info(message)
+        return message
+
+    # Edited command is valid
+    if len(command) > 2:
+        message = 'Edited custom command %s' % command[1]
+        custom_commands.edit_command(bot.streamer, command[1], command[2:])
+        LOGGER.info(message)
+        LOGGER.debug(bot.commands['custom'])
+        return message
+
+    # Edited command doesn't have output
+    message = 'Custom commands require a message to send to chat when called'
+    LOGGER.info(message)
+    return message
+
+COMMANDS = {
+    '!addcom': partial(add_command),
+    '!delcom': partial(delete_command),
+    '!editcom': partial(edit_command)
+    }
+
+
 def do_default_command(twitch_bot, command):
-    try:
-        command_name = command[0]
-        custom_command_name = command[1]
-    except IndexError:
+    """Calls the command function the user invokes."""
+    if len(command) < 2:
         message = 'Custom command name not provided'
+        LOGGER.info(message)
+        return message
+
+    try:
+        return COMMANDS[command[0]](twitch_bot, command)
+    except KeyError:
+        message = 'Default command name not found'
         LOGGER.error(message)
-        return message
-
-    if command_name == '!addcom':
-        LOGGER.debug('Add Command command received')
-
-        # Command exists
-        if (custom_command_name in twitch_bot.commands['default']
-                + twitch_bot.commands['guessing-game']):
-            message = 'Command %s already exists' % custom_command_name
-            LOGGER.info(message)
-            return message
-
-        # New command is valid
-        if len(command) > 2:
-            message = 'Added custom command %s' % custom_command_name
-            custom_commands.add_command(twitch_bot.streamer, custom_command_name, command[2:])
-            twitch_bot.commands['custom'] += [custom_command_name]
-            LOGGER.info('%s with output %s', message, ' '.join(command[2:]))
-            LOGGER.debug(twitch_bot.commands['custom'])
-            return message
-
-        # New command doesn't have output
-        message = 'Custom commands require a message to send to chat when called'
-        LOGGER.info(message)
-        return message
-
-    elif command_name == '!delcom':
-        LOGGER.debug('Delete Command command received')
-
-        # Built-in command cannot be deleted
-        if (custom_command_name in twitch_bot.commands['default']
-                + twitch_bot.commands['guessing-game']):
-            message = 'Command %s cannot be removed' % custom_command_name
-            LOGGER.info(message)
-            return message
-
-        # Command doesn't exist
-        if not custom_command_name in twitch_bot.commands['custom']:
-            message = 'Command %s does not exist' % custom_command_name
-            LOGGER.info(message)
-            return message
-
-        # Delete given command
-        message = 'Removed custom command %s' % custom_command_name
-        custom_commands.remove_command(twitch_bot.streamer, custom_command_name)
-        twitch_bot.commands['custom'].remove(custom_command_name)
-        LOGGER.info(message)
-        LOGGER.debug(twitch_bot.commands['custom'])
-        return message
-
-    elif command_name == '!editcom':
-        LOGGER.debug('Edit Command command received')
-
-        # Built-in commands cannot be edited
-        if (custom_command_name in twitch_bot.commands['default']
-                + twitch_bot.commands['guessing-game']):
-            message = 'Command %s cannot be edited' % custom_command_name
-            LOGGER.info(message)
-            return message
-
-        # Command doesn't exist
-        if not custom_command_name in twitch_bot.commands['custom']:
-            message = 'Command %s does not exist' % custom_command_name
-            LOGGER.info(message)
-            return message
-
-        # Edited command is valid
-        if len(command) > 2:
-            message = 'Edited custom command %s' % custom_command_name
-            custom_commands.edit_command(twitch_bot.streamer, custom_command_name, command[2:])
-            LOGGER.info(message)
-            LOGGER.debug(twitch_bot.commands['custom'])
-            return message
-
-        # Edited command doesn't have output
-        message = 'Custom commands require a message to send to chat when called'
-        LOGGER.info(message)
         return message
