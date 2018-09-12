ba303fa7b077d3708be49ceaaf463c474378cb01
Vincent Cunningham <v.cunningham@flagrama.com>
2018-09-11 21:09:19 -0400

    fix setup.py to use the correct directory name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup # pylint: disable=no-name-in-module,import-erro
 setup(
     name='GuessingGameBot',
     version='1.1-dev',
-    packages=['guessing-game-bot'],
+    packages=['guessing_game_bot'],
     install_requires=[
         "requests==2.18.4",
         "boto3==1.9.0",
